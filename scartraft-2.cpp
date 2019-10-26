de17282339e221b881a1f7d1c4e1a3a0fc018b4e
Marcelo Morais <marcelodotmorais@gmail.com>
2019-10-26 17:38:51 -0300

    feat: display terran background instead of text

--- scartraft-2.cpp
+++ scartraft-2.cpp
@@ -1,19 +1,24 @@
 #include <allegro5/allegro.h>
 #include <allegro5/allegro_ttf.h>
 #include <allegro5/allegro_font.h>
+#include <allegro5/allegro_image.h>
+#include <allegro5/events.h>
 
 int main()
 {
 	al_init();
-	al_init_font_addon();
-	al_init_ttf_addon();
 
-	ALLEGRO_DISPLAY* display = al_create_display(640, 480);
-	ALLEGRO_FONT* font = al_load_ttf_font("YARDSALE.ttf", 64, 0);
+	auto display = al_create_display(800, 600);
+
+	//auto eventQueue = al_create_event_queue();
+	//al_register_event_source(eventQueue, al_get_display_event_source(display));
+	
+	al_init_image_addon();
+
+	auto background = al_load_bitmap("assets/background/terran.tga");
 
 	while (true) {
-		al_clear_to_color(al_map_rgb(255, 255, 255));
-		al_draw_text(font, al_map_rgb(0, 0, 0), 0, 0, 0, "Hello World");
+		al_draw_bitmap(background, 0, 0, 0);		
 		al_flip_display();
 	}
 }
